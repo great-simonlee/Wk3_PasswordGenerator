c4328a70c6c7556f50b3e769962dbd7ce59f40c2
DS_Portfolio <great.simonlee@gmail.com>
2020-08-18 19:29:04 -0400

    Prompt added

--- example/Develop/script.js
+++ example/Develop/script.js
@@ -11,7 +11,11 @@ function writePassword() {
 }
 
 function generatePassword () {
-  prompt("Hello world");
+  var passwordLength = prompt("How many letters do you want in your password? (8-128)");
+  while (passwordLength < 8 || passwordLength > 128) {
+  	window.alert("Password should be greater than 8 letter and less than 128 letters");
+  	prompt("How many letters do you want in your password? (8-128)");
+  };
 };
 
 
@@ -23,6 +27,21 @@ generateBtn.addEventListener("click", writePassword);
 // WHEN I click the button to generate a password
 // THEN I am presented with a series of prompts for password criteria
 
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
 
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
 
+// WHEN prompted for character types to include in the password
+// THEN I choose lowercase, uppercase, numeric, and/or special characters
 
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type
+// should be selected
+
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
